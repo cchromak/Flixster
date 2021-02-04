6c584876486f677f99a2b2c144405fd78255e7f2
cchromak <cchromak@gmail.com>
2021-02-04 14:28:53 -0500

    Added Movie class, function to return list of movies from JSONArray of movies

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -7,14 +7,24 @@ import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
 
-    /* create a constant of the endpoint for the IMDB API*/
+    /* create a constant of the endpoint for the IMDB API
+    * TAG created to test via log */
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
+
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +40,23 @@ public class MainActivity extends AppCompatActivity {
         client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int i, Headers headers, JSON json) {
+                // logging out to check if successful or not.
                 Log.d(TAG, "onSuccess");
+                // returns the actual jsonObject
+                JSONObject jsonObject = json.jsonObject;
+                // Try => get corresponding jsonArray with key: 'results'
+                // must use catch in case the key: 'results' doesn't exist. handles error
+                // Log.e => log error, Log.i => log info
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results.toString());
+                    // Return list of Movie objects
+                    movies = Movie.fromJsonArray(results);
+                    Log.i(TAG, "Movies: " + movies.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit json exception");
+                }
+
             }
 
             @Override
